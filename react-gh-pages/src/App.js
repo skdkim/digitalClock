ef36e79c1ff1372de31da0c777f0e5263300dcda
Sung Kwang David Kim <sungkwangkim@gmail.com>
2018-08-22 19:40:20 -0700

    reset everything

--- react-gh-pages/src/App.js
+++ react-gh-pages/src/App.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import Clock from './Clock';
 
 class App extends Component {
   render() {
@@ -9,10 +8,10 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to Clock</h1>
+          <h1 className="App-title">Welcome to React</h1>
         </header>
         <p className="App-intro">
-          <Clock />
+          To get started, edit <code>src/App.js</code> and save to reload.
         </p>
       </div>
     );
